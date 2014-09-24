39653c60a0ea4540df267078e3635abd7e3cf778
Niels Wijk <niels@thegreatnew.nl>
2014-09-24 11:45:09 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='TinyCSV',
+    version='0.1',
+    description='Tiny CSV parser',
+    author='Niels Wijk',
+    url='https://github.com/newijk/tinycsv/',
+    py_modules=['tinycsv'],
+)
